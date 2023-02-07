230e7c4df2c8f284aba198fce3b37a4a8d21c956
Prayash Koirala <prayashkoirala07@gmail.com>
2023-02-07 14:25:26 +0545

    initial commit

---
+++ android/app/src/main/kotlin/com/example/api_handling_with_clean_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.api_handling_with_clean_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/posts/presentation/bloc/posts_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'posts_event.dart';
+part 'posts_state.dart';
+
+class PostsBloc extends Bloc<PostsEvent, PostsState> {
+  PostsBloc() : super(PostsInitial()) {
+    on<PostsEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
